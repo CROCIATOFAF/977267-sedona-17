563007a6b20b9491bd64a4f66cb94458687887c8
CROCIATOFAF <48692205+CROCIATOFAF@users.noreply.github.com>
2019-08-12 22:54:58 +0300

    Вносит правки

--- source/js/blocks/main-nav.js
+++ source/js/blocks/main-nav.js
@@ -1,5 +1,5 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navToggle = document.querySelector('.menu-open__toggle');
 navMain.classList.remove('main-nav--nojs');
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
